33f45b818fd04f621a95879a41e8d36fa480042a
diarih <diarihza@gmail.com>
2023-09-04 22:37:50 +0700

    feat: added infinite scroll, added react-intersection lib

---
+++ src/app/components/cards/PokemonCard.tsx
@@ -0,0 +1,38 @@
+import Image from 'next/image'
+import React from 'react'
+
+export default function PokemonCard({ data }: { data: Pokemon }) {
+
+    const cardBody = (
+        <a href={`/${data.id}`}>
+            <div style={{ height: 200 }} className='bg-base-300 rounded w-full p-4 flex justify-center items-center'>
+                <div className='relative' style={{height: 150, width: 150}}>
+                    <Image alt="pokemon" fill  src={data.sprites.other.dream_world.front_default} />
+                </div>
+            </div>
+            <div className='p-4'>
+                <div className='text-xs font-semibold mb-3'>
+                    #{data.id}
+                </div>
+                <div>
+                    <div className='text-xl font-semibold mb-2 capitalize'>{data.name}</div>
+                    <div className='flex items-center gap-2'>
+                        {
+                            data.types.map((type, c: number) => {
+                                return (
+                                    <div key={c} className="badge badge-info font-semibold text-xs capitalize">{type.type.name}</div>
+                                )
+                            })
+                        }
+                    </div>
+                </div>
+            </div>
+        </a>
+    )
+
+    return (
+        <article className='md:col-span-3 col-span-6 flex flex-col gap-3 rounded bg-base-200'>
+            {cardBody}
+        </article>
+    )
+}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,6 +1,8 @@
 import Head from './components/sections/Head'
 import List from './components/sections/List'
 
+
+
 export default function Home() {
   return (
     <main className="container mx-auto px-4 py-12 bg-neutral">
